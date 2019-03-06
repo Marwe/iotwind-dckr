88df0ea2df1dd58bc19d0642968a2f96f642c0f6
Martin Weis <mwgit.20.mweis@spamgourmet.com>
2019-03-06 14:25:48 +0100

    credits title

--- plot_windroses.R
+++ plot_windroses.R
@@ -10,11 +10,12 @@ d$timestamp=as.POSIXct(d$time/1000/1000/1000, origin = "1970-01-01", tz = "UTC")
 # windRose(d, ws="wind_speed", wd="wind_direction")
 #ggplot(d,aes(wind_angle,wind_speed))+geom_point()
 
-
 pdf("windrose_plots.pdf")
 for (cmpgn in unique(d$campaign)){
     for (dvcd in unique(d$deviceid)){
         
+    # credits for the title changing solution by
+    # https://stackoverflow.com/questions/18376679/removing-default-title-from-wind-rose-in-openair-package
         my.statistic <- list("fun"=length,"unit" = "%","scale" = "all", "lab" = paste("campaign:",cmpgn, "deviceid:",dvcd) , "fun2" = function(x) signif(mean(x, na.rm = TRUE), 3), "lab2" = "mean","labcalm" = function(x) round(x, 1))
 
         windRose(d[d$deviceid==dvcd&d$campaign==cmpgn,], ws="WGA", wd="WRA", angle=10,statistic=my.statistic)
